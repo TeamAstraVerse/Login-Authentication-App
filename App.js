5e9847cd3fc7376e00880003b974c0963e740af6
mahendrasaikumargandham <mahendragandham730@gmail.com>
2022-06-26 16:45:02 +0530

    Added Code

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen options = {{ headerShown: false }} name = "Login" component = {LoginScreen} />
+          <Stack.Screen name = "Home" component = {HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
